014e673ffa9888b04428b863e0266bb0592221cb
Augusto C S Silva <augustosumac@gmail.com>
2023-12-15 01:16:43 -0300

    Final config swagger + map methods in Auth Controller

--- src/app/Http/Controllers/AuthController.php
+++ src/app/Http/Controllers/AuthController.php
@@ -6,30 +6,79 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+use OpenApi\Annotations as OA;
+
+/**
+ * @OA\Schema(
+ *     schema="User",
+ *     title="User",
+ *     description="User model",
+ *     @OA\Property(property="id", type="integer", description="User ID"),
+ *     @OA\Property(property="name", type="string", description="User name"),
+ *     @OA\Property(property="email", type="string", format="email", description="User email"),
+ *     @OA\Property(property="created_at", type="string", format="date-time", description="Creation timestamp"),
+ *     @OA\Property(property="updated_at", type="string", format="date-time", description="Last update timestamp")
+ * )
+ */
+
 
 class AuthController extends Controller
 {
-    // Método para registro de usuário
+    /**
+     * @OA\Post(
+     *     path="/api/register",
+     *     summary="Register a new user",
+     *     description="Register a new user with the provided data.",
+     *     tags={"Authentication"},
+     *     @OA\RequestBody(
+     *         required=true,
+     *         @OA\JsonContent(
+     *             required={"name", "email", "password"},
+     *             @OA\Property(property="name", type="string", example="John Doe"),
+     *             @OA\Property(property="email", type="string", format="email", example="john@example.com"),
+     *             @OA\Property(property="password", type="string", format="password", example="secret")
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=201,
+     *         description="User registered successfully",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="access_token", type="string", description="Access token for authentication"),
+     *             @OA\Property(property="token_type", type="string", description="Token type (Bearer)"),
+     *             @OA\Property(property="user", ref="#/components/schemas/User")
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=422,
+     *         description="Validation error",
+     *         @OA\JsonContent(
+     *             type="object",
+     *             @OA\Property(property="errors", type="object", description="Validation errors")
+     *         )
+     *     )
+     * )
+     */
     public function register(Request $request)
     {
-        // Validação dos dados de entrada
-        $validatedData = $request->validate([
+        $validatedData = Validator::make($request->all(), [
             'name' => 'required|max:255',
             'email' => 'required|email|unique:users',
             'password' => 'required|min:6'
         ]);
 
-        // Criação do usuário
+        if ($validatedData->fails()) {
+            return response()->json(['errors' => $validatedData->errors()], 422);
+        }
+
         $user = User::create([
-            'name' => $validatedData['name'],
-            'email' => $validatedData['email'],
-            'password' => Hash::make($validatedData['password']),
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
         ]);
 
-        // Criação do token
         $token = $user->createToken('auth_token')->plainTextToken;
 
-        // Resposta JSON
         return response()->json([
             'access_token' => $token,
             'token_type' => 'Bearer',
@@ -37,27 +86,53 @@ class AuthController extends Controller
         ]);
     }
 
-    // Método para login de usuário
+    /**
+     * @OA\Post(
+     *     path="/api/login",
+     *     summary="User login",
+     *     description="Login a user with email and password.",
+     *     tags={"Authentication"},
+     *     @OA\RequestBody(
+     *         required=true,
+     *         @OA\JsonContent(
+     *             required={"email", "password"},
+     *             @OA\Property(property="email", type="string", format="email", example="john@example.com"),
+     *             @OA\Property(property="password", type="string", format="password", example="secret")
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="User logged in successfully",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="access_token", type="string", description="Access token for authentication"),
+     *             @OA\Property(property="token_type", type="string", description="Token type (Bearer)"),
+     *             @OA\Property(property="user", ref="#/components/schemas/User")
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=401,
+     *         description="Invalid credentials",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="message", type="string", description="Error message")
+     *         )
+     *     )
+     * )
+     */
     public function login(Request $request)
     {
-        // Validação dos dados de entrada
         $validatedData = $request->validate([
             'email' => 'required|email',
             'password' => 'required'
         ]);
 
-        // Verifica as credenciais
         if (!Auth::attempt($validatedData)) {
-            return response()->json(['message' => 'Invalid credentials'], 401);
+            return response()->json(['message' => 'Invalid credentials']);
         }
 
-        // Obtém o usuário autenticado
         $user = User::where('email', $validatedData['email'])->firstOrFail();
 
-        // Criação do token
         $token = $user->createToken('auth_token')->plainTextToken;
 
-        // Resposta JSON
         return response()->json([
             'access_token' => $token,
             'token_type' => 'Bearer',
@@ -65,10 +140,31 @@ class AuthController extends Controller
         ]);
     }
 
-    // Método para logout de usuário
+    /**
+     * @OA\Post(
+     *     path="/api/logout",
+     *     summary="User logout",
+     *     description="Logout the authenticated user.",
+     *     tags={"Authentication"},
+     *     security={{ "sanctum": {} }},
+     *     @OA\Response(
+     *         response=200,
+     *         description="User logged out successfully",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="message", type="string", description="Success message")
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=401,
+     *         description="Unauthenticated",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="message", type="string", description="Error message")
+     *         )
+     *     )
+     * )
+     */
     public function logout(Request $request)
     {
-        // Revoga todos os tokens do usuário autenticado
         $request->user()->tokens()->delete();
 
         return response()->json(['message' => 'Logged out successfully']);
--- src/app/Http/Controllers/Controller.php
+++ src/app/Http/Controllers/Controller.php
@@ -5,12 +5,19 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use OpenApi\Annotations as OA;
 
 /**
  * @OA\Info(
  *     title="API de Ordens de Serviço - Jump Park",
  *     description="Documentação da API de Ordens de Serviço - Jump Park",
  *     version="1.0"
+ * )
+ * @OA\SecurityScheme(
+ *     type="http",
+ *     securityScheme="sanctum",
+ *     scheme="bearer",
+ *     bearerFormat="JWT"
  * )
  */
 
